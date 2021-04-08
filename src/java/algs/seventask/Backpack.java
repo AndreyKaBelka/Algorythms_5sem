40ef98189e25d84f387b6713c2e588155f8096ce
Полубояринов Сергей <s.poluboyarinov911@gmail.com>
2021-04-08 22:05:35 +0600

    complete 7 task

---
+++ src/java/algs/seventask/Backpack.java
@@ -0,0 +1,23 @@
+package algs.seventask;
+
+class Item {
+    public  String name;
+    public  double weigth;
+    public  double price;
+
+    @Override
+    public String toString() {
+        return "Item{" +
+                "name='" + name + '\'' +
+                ", weigth=" + weigth +
+                ", price=" + price +
+                '}';
+    }
+
+    public Item(String name, double weight, double price)
+    {
+        this.name = name;
+        this.weigth = weight;
+        this.price = price;
+    }
+}
