92286c3c8b89322b636b71439fb861f32394549e
Полубояринов Сергей <s.poluboyarinov911@gmail.com>
2021-05-08 21:19:47 +0600

    sixth task

---
+++ src/java/algs/sixth/RBTmain.java
@@ -0,0 +1,16 @@
+package algs.sixth;
+
+public class RBTmain {
+    static RBTree<Integer> tree;
+    private static final int a[] = {10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120};
+
+    public static void main(String[] args) {
+        int i, ilen = a.length;
+        tree = new RBTree<Integer>();
+        for (i = 0; i < ilen; i++) {
+            tree.insert(a[i]);
+        }
+
+        tree.print();
+    }
+}
